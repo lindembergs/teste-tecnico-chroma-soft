5c0e5453bf0e18acab6f5c9e1b31dd60b21939c4
Lindemberg <lindembergparaiba@gmail.com>
2025-01-11 21:38:14 -0300

    style: adicao do footer no global css

--- pages/create_user.php
+++ pages/create_user.php
@@ -10,7 +10,7 @@ require '../includes/connect.php';
     <title>Criar Usuário - Sistema</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/create_user.css">
     <link rel="stylesheet" href="../assets/css/global.css">
 </head>
 <body class="bg-light">
