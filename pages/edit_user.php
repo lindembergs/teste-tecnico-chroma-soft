5b19f66f68adc8390b50f93fcdd79590f729ae8f
Lindemberg <lindembergparaiba@gmail.com>
2025-01-11 22:15:08 -0300

    refactor: alt nas linguagens de en para pt-BR

--- pages/edit_user.php
+++ pages/edit_user.php
@@ -3,7 +3,7 @@ session_start();
 require '../includes/connect.php';
 ?>
 <!doctype html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
--- pages/view_user.php
+++ pages/view_user.php
@@ -2,7 +2,7 @@
 require '../includes/connect.php';
 ?>
 <!doctype html>
-<html lang="en">
+<html lang="PT-BR">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
