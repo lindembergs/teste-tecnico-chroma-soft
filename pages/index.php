1bf5f09ea9c35db8ec6729812c7dcc44332b4fff
Lindemberg <lindembergparaiba@gmail.com>
2025-01-11 22:13:59 -0300

    refactor: alt no nome de style para index css

--- pages/index.php
+++ pages/index.php
@@ -10,7 +10,7 @@ require '../includes/connect.php';
     <title>Lista de usuários</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/index.css">
     <link rel="stylesheet" href="../assets/css/global.css">
   </head>
   <body>
