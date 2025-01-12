f51ed999f8f2818bae8093411fa38d18663ea83e
Lindemberg <lindembergparaiba@gmail.com>
2025-01-11 21:59:42 -0300

    refactor: remocao do style css do edit_user

--- pages/edit_user.php
+++ pages/edit_user.php
@@ -7,7 +7,7 @@ require '../includes/connect.php';
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Usuário - Editar</title>
+    <title>Edição de usuário</title>
     <link 
     
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
@@ -15,7 +15,6 @@ require '../includes/connect.php';
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
         crossorigin="anonymous">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-    <!-- <link rel="stylesheet" href="../assets/css/style.css"> -->
     <link rel="stylesheet" href="../assets/css/global.css">
 </head>
 <body>
