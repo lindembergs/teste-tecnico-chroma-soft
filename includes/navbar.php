1787eca29bd2bdaf7e95aac14bb51083a2a1654b
Lindemberg <lindembergparaiba@gmail.com>
2025-01-12 23:08:12 -0300

    feat: add tag ancor para o header voltar para a pagina inicial

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-dark bg-dark">
 <div class="container-md header">
-    <img src="../assets/img/logo.png" alt="Logo">
+    <a href="../pages/index.php"><img src="../assets/img/logo.png" alt="Logo"></a>
 </div>
 </nav>
